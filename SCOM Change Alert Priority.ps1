7cec8d25349744b004b10b56e399c65732974679
G20_100 <akifumi@yoshikawa.in>
2020-03-17 15:17:26 +0900

    Add files via upload

---
+++ SCOM Change Alert Priority.ps1
@@ -0,0 +1,46 @@
+﻿Import-Module -Name "OperationsManager" 
+New-SCManagementGroupConnection -ComputerName "scom12jpn-ms1"
+
+$AlertCsvFile = "C:\scripts\alertcsvfile.csv"
+$ManagementPackName = "test.override.mp"
+
+$monitors = Get-SCOMMonitor |Where-Object {$_.alertsettings -ne $null}
+#$monitors
+#$Rules = get-scomrule | Where-Object {$_.WriteActionCollection.Name -eq "GenerateAlert"}
+
+$AlertList = Import-Csv $AlertCsvFile
+$AlertListCount = ($AlertList | Measure-Object).count
+#$AlertList
+
+$MP = Get-SCOMManagementPack -name $ManagementPackName
+#$MP
+
+for($i = 0; $i -lt $AlertListCount ; $i++){
+#    Write-Output "Forin"
+    $AlertName = $AlertList[$i].AlertName.Tostring()
+    $AlertName
+    $MonitorTemp = $monitors | ? {$_.AlertSettings.AlertMessage.getElement().Displayname -eq "$AlertName"}
+#    $Monitor = $MonitorTemp[0]
+
+
+    if ($Monitor.AlertSettings.AlertPriority -eq "Normal")
+    {
+    foreach ($Monitor in $MonitorTemp)
+    {
+    $MonitorName = $Monitor.Name
+#    Write-Output "IfIn"
+    $Class = Get-ScomClass -id $Monitor.Target.id
+    $OverrideName = "OV_" + $MonitorName
+    $MonitorOverride = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackMonitorPropertyOverride($MP, $OverrideName)
+    $MonitorOverride.Monitor = $Monitor
+    $MonitorOverride.Property = "AlertPriority"
+    $MonitorOverride.Value = "High"
+    $MonitorOverride.Context = $Class
+    }
+    }
+}
+
+$MP.Verify()
+$MP.AcceptChanges()
+
+Get-SCOMOverride |? {$_.name -like "OV_*"}
---
+++ get-ScomMonitorOverrideParam.ps1
@@ -0,0 +1,39 @@
+﻿param(
+[Parameter(Mandatory=$true)]
+[string]
+$ScomManagementServer
+)
+
+function GetScomMonitorOverridenParams(
+                                     [String] $MonitorName,
+                                     [String] $MonitorDisplayName,
+                                     [String] $MonitorTarget,
+                                     [String] $MonitorConfig,
+                                     [String] $MonitorOverridenParamaters
+                                   ) {
+
+    $Object = New-Object PSObject;
+    $Object | Add-Member -MemberType NoteProperty -Name "MonitorName" $MonitorName;
+    $Object | Add-Member -MemberType NoteProperty -Name "MonitorDisplayName" $MonitorDisplayName;
+    $Object | Add-Member -MemberType NoteProperty -Name "MonitorTarget" $MonitorTarget;
+    $Object | Add-Member -MemberType NoteProperty -Name "MonitorConfig" $MonitorConfig;
+    $Object | Add-Member -MemberType NoteProperty -Name "MonitorOverridenParamater" $MonitorOverridenParamaters;
+
+    return $Object
+}
+
+# module Load and Connection
+import-module OperationsManager
+New-SCOMManagementGroupConnection $ScomManagementServer
+
+$ScomMonitors = Get-SCOMMonitor |? {$_.XmlTag -eq 'UnitMonitor'}
+
+$ScomMonitorsOverRidenParams = @()
+
+foreach ($ScomMonitor in $ScomMonitors)
+{
+    $ScomMonitorsOverRidenParams += GetScomMonitorOverridenParams $ScomMonitor.Name $ScomMonitor.DisplayName $ScomMonitor.Target.Identifier.Path $ScomMonitor.Configuration $ScomMonitor.GetOverrideableParameters().name;
+}
+
+$ScomMonitorsOverRidenParams
+
